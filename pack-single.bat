02e162e87b1a9a44ffd3b57ce78683cf602e03bd
Matt Garrish <matt.garrish@gmail.com>
2012-03-14 23:46:46 +0000

    modified the pack batch file to auto-append the current date to the epub name

--- pack-single.bat
+++ pack-single.bat
@@ -1,2 +1,11 @@
-@echo off
+@echo off & setLocal EnableDelayedExpansion
+
+for /F "tokens=1,2,3,4 delims=/ " %%A in ('Date /t') do @( 
+	set fullDate=%%D%%B%%C
+)
+
+set fileName=%~n1
+set fileName=!fileName: =_!
+
 java -jar lib/epubcheck-3.0b4/epubcheck-3.0b4.jar %1 -mode "exp" -save
+move !fileName!.epub !fileName!-!fullDate!.epub
