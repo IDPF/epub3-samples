e5a19a5e7dd6e1d4d0f301f2390b8a8aef36986d
markus.gylling@gmail.com <markus.gylling@gmail.com@53b911a0-7416-2ca7-a09b-f29e08e9fc06>
2012-04-17 11:43:33 +0000

    Add SVG FXL from Takeshi, update epubcheck

--- trunk/pack-single.bat
+++ trunk/pack-single.bat
@@ -7,5 +7,5 @@ for /F "tokens=1,2,3 delims=/ " %%A in ('Date /t') do @(
 set fileName=%~n1
 set fileName=!fileName: =_!
 
-java -jar lib/epubcheck-3.0b4/epubcheck-3.0b4.jar %1 -mode "exp" -save
+java -jar lib/epubcheck-3.0b5-pre/epubcheck-3.0b5-pre.jar %1 -mode "exp" -save
 move !fileName!.epub !fileName!-!fullDate!.epub
--- trunk/validate-single.bat
+++ trunk/validate-single.bat
@@ -1,2 +1,2 @@
 @echo off
-java -jar lib/epubcheck-3.0b4/epubcheck-3.0b4.jar %1
+java -jar lib/epubcheck-3.0b5-pre/epubcheck-3.0b5-pre.jar %1
