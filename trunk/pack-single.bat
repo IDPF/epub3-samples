4ae1adc3c7275f2ec0c62b2572ca3609bdddc142
mgarrish <mgarrish@53b911a0-7416-2ca7-a09b-f29e08e9fc06>
2013-07-27 00:18:22 +0000

    adding epubcheck 3.0.1 and updating win bat files to run

--- trunk/pack-single.bat
+++ trunk/pack-single.bat
@@ -7,5 +7,5 @@ for /F "tokens=1,2,3 delims=/ " %%A in ('Date /t') do @(
 set fileName=%~n1
 set fileName=!fileName: =_!
 
-java -jar lib/epubcheck-3.0-RC-2/epubcheck-3.0-RC-2.jar %1 -mode "exp" -save
-move !fileName!.epub !fileName!-!fullDate!.epub
+java -jar lib/epubcheck-3.0.1/epubcheck-3.0.1.jar %1 -mode "exp" -save
+move 30\!fileName!.epub !fileName!-!fullDate!.epub
